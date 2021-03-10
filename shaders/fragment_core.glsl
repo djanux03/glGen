3b31eae05374031c67b7a1ebfe16da2e57368650
djanux03 <djanux03@gmail.com>
2021-03-10 18:23:02 +0100

    textures

--- shaders/fragment_core.glsl
+++ shaders/fragment_core.glsl
@@ -2,9 +2,13 @@
 out vec4 FragColor;
 
 in vec3 ourColor;
+in vec2 texCoord;
 
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 void main()
 {
-    FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
-    
+    //FragColor = vec4(0.9f, 0.9f, 0.9f, 1.0f);
+    FragColor = texture(texture1, texCoord);
+    FragColor = texture(texture2, texCoord);
 }
--- shaders/vertex_core.glsl
+++ shaders/vertex_core.glsl
@@ -2,9 +2,10 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 layout (location = 2) in vec2 aTextureCoord;
-
+layout (location = 3) in vec2 secondTexCoord;
 out vec3 ourColor;
-out vec2 textCoord;
+out vec2 texCoord;
+out vec2 secondTexCoord;
 uniform mat4 transform; // set in code
 
 void main()
@@ -13,6 +14,6 @@ void main()
 	gl_Position = vec4(aPos, 1.0);
 	ourColor = aColor;
 
-	textCoord = aTextureCoord;
-
+	texCoord = aTextureCoord;
+	secondTexCoord = secondTexCoord;
 }
