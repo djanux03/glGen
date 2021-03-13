39d430b786070834c9cb82afb8deecc144b15fc6
djanux03 <djanux03@gmail.com>
2021-03-13 22:02:56 +0100

    commitmain

--- shaders/fragment_core.glsl
+++ shaders/fragment_core.glsl
@@ -5,10 +5,10 @@ in vec3 ourColor;
 in vec2 texCoord;
 
 uniform sampler2D texture1;
-uniform sampler2D texture2;
+//uniform sampler2D texture2;
 void main()
 {
     //FragColor = vec4(0.9f, 0.9f, 0.9f, 1.0f);
     FragColor = texture(texture1, texCoord);
-    FragColor = texture(texture2, texCoord);
+    //FragColor = texture(texture2, texCoord);
 }
--- shaders/vertex_core.glsl
+++ shaders/vertex_core.glsl
@@ -2,10 +2,10 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 layout (location = 2) in vec2 aTextureCoord;
-layout (location = 3) in vec2 secondTexCoord;
+//layout (location = 3) in vec2 secondTexCoord;
 out vec3 ourColor;
 out vec2 texCoord;
-out vec2 secondTexCoord;
+//out vec2 secondTexCoord;
 uniform mat4 transform; // set in code
 
 void main()
@@ -15,5 +15,5 @@ void main()
 	ourColor = aColor;
 
 	texCoord = aTextureCoord;
-	secondTexCoord = secondTexCoord;
+	//secondTexCoord = secondTexCoord;
 }
